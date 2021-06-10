1c65795a5579a17e12f065a954c6c99964e82795
troy213 <tritera.erlangga@gmail.com>
2021-06-10 12:11:38 +0700

    express api

---
+++ data/product.js
@@ -0,0 +1,34 @@
+const product = [
+  {
+    id: 1,
+    name: 'product 1',
+    price: 1,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium expedita, fuga, sed odio error iure ex laboriosam quibusdam rerum eligendi odit. Illo quia odio porro ex aperiam necessitatibus exercitationem eveniet.',
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    price: 2,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium expedita, fuga, sed odio error iure ex laboriosam quibusdam rerum eligendi odit. Illo quia odio porro ex aperiam necessitatibus exercitationem eveniet.',
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    price: 3,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium expedita, fuga, sed odio error iure ex laboriosam quibusdam rerum eligendi odit. Illo quia odio porro ex aperiam necessitatibus exercitationem eveniet.',
+  },
+  {
+    id: 4,
+    name: 'product 4',
+    price: 4,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium expedita, fuga, sed odio error iure ex laboriosam quibusdam rerum eligendi odit. Illo quia odio porro ex aperiam necessitatibus exercitationem eveniet.',
+  },
+  {
+    id: 5,
+    name: 'product 5',
+    price: 5,
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium expedita, fuga, sed odio error iure ex laboriosam quibusdam rerum eligendi odit. Illo quia odio porro ex aperiam necessitatibus exercitationem eveniet.',
+  },
+]
+
+module.exports = product
